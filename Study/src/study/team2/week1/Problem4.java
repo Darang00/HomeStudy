67adba9a5ebf34791da99a5ac0f6480f92d0228e
노다영 <noda21@naver.com>
2022-09-04 13:03:22 +0900

    study

---
+++ Study/src/study/team2/week1/Problem4.java
@@ -0,0 +1,11 @@
+package study.team2.week1;
+
+public class Problem4 {
+	//4. "안녕하세요"라고 표시하는 hello 메서드 작성. 7-5 (270p) !!!main 함수에서 scanner와 반복문을 사용하여, hello 메소드를 10번 이상 호출하여 출력하기)!!!!
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Study/src/study/team2/week1/Problem5.java
@@ -0,0 +1,11 @@
+package study.team2.week1;
+
+public class Problem5 {
+	//5. 1부터 n까지의 정수의 합을 구해서 반환하는 메서드 작성. 7-4 (268p) //메서드 작성시 패키지는 각자 마음대로.
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
